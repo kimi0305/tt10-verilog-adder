e7deaabf976c2c68fe779b6e73527d5a62acd6ed
kimi0305 <ivanni0305@gmail.com>
2025-02-21 18:15:58 -0500

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  priority_encoder user_project (
+  matching_inverter user_project (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
